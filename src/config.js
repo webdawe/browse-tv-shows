86abcab956e2fae2519823b00b3e2868e343a21b
Anil Paul <anilnpaul@gmail.com>
2025-03-03 15:53:12 +1100

    dotenv

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY_PARAM = "?api_key=8cfe0d4fd66e6f362e0e38b5810c1f70";
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original";
 const SMALL_IMAGE_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300";
+const API_KEY_PARAM = import.meta.env.VITE_API_KEY_PARAM;
 export {
   BASE_URL,
-  API_KEY_PARAM,
   BACKDROP_BASE_URL,
   SMALL_IMAGE_COVER_BASE_URL,
+  API_KEY_PARAM,
 };
