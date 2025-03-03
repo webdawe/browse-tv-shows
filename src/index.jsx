05b177aa5a82ab7450e0c4d01053b7e0dca4fef1
Anil Paul <anilnpaul@gmail.com>
2025-03-03 13:15:25 +1100

    strict mode

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
-import "./global.css"
+import "./global.css";
 // Import some react tools
 import ReactDOM from "react-dom/client";
 // Import our first component : App
 import { App } from "./App";
+import { StrictMode } from "react";
 // Target the root div
 const rootDiv = document.getElementById("root");
 
@@ -10,4 +11,8 @@ const rootDiv = document.getElementById("root");
 const reactRoot = ReactDOM.createRoot(rootDiv);
 
 // Inject our App component into the react node
-reactRoot.render(<App />);
+reactRoot.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
